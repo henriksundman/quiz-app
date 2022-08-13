f779c7c94086f8c56a70b13f4bf7601db8997703
Henrik Sundman <henriksundman@yahoo.se>
2022-08-13 16:31:29 +0200

    refactor Question add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,9 @@
-import axios, { AxiosResponse } from 'axios';
-import React, { useEffect, useState } from 'react';
+import Question from './components/Question';
 
-import { fetchQuestions } from './utils/fetchQuestions';
-interface Question {
-	category: string;
-	id: string;
-	correctAnswer: string;
-}
+import './app.css';
 
 function App() {
-	const [questions, setQuestions] = useState([]);
-
-	useEffect(() => {
-		fetchQuestions().then((response: AxiosResponse) => {
-			setQuestions(response.data);
-		});
-	}, []);
-
-	console.log(questions);
-	return (
-		<ul>
-			{questions.map((q: Question) => (
-				<li key={q.id}>
-					{q.category} {q.correctAnswer}
-				</li>
-			)) || 'Loading'}
-		</ul>
-	);
+	return <Question />;
 }
 
 export default App;
-
-// [
-//   {
-//     "category": "History",
-//     "id": "622a1c367cc59eab6f950316",
-//     "correctAnswer": "Mumtaj Mahal",
-//     "incorrectAnswers": [
-//       "Taj Bodhi",
-//       "Lugah Taj",
-//       "Mahal Mahore"
-//     ],
-//     "question": "Who was the Taj Mahal built in memory of?",
-//     "tags": [
-//       "tourist_attractions",
-//       "india",
-//       "history"
-//     ],
-//     "type": "Multiple Choice",
-//     "difficulty": "hard",
-//     "regions": []
-//   },
