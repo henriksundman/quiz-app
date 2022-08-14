a9731ee3df1646516f068152293f3d514916d710
Henrik Sundman <henriksundman@yahoo.se>
2022-08-14 15:47:03 +0200

    add Card update global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import './app.css';
 import Question from './components/Question';
 
-import './app.css';
+import Card from './components/ui/Card';
+
+const DUMMY_TEXT =
+	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque maxime corporis animi quibusdam, velit eos laudantium laborum reiciendis incidunt explicabo aut aperiam commodi soluta rem eius iste qui, saepe repellat sequi. Adipisci expedita velit rerum fugiat a aut sapiente quibusdam.';
 
 function App() {
-	return <Question />;
+	return <Card>{DUMMY_TEXT}</Card>;
 }
 
 export default App;
